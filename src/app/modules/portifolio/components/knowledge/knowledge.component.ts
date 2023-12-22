89ccc1fc988ae019d289c4f81431002fe7d0be59
hc-pedrosouza <hc.pedrosouza@gmail.com>
2023-12-22 11:10:55 -0300

    feat: add knowledge icons

--- src/app/modules/portifolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portifolio/components/knowledge/knowledge.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IKnowledge } from '../../interface/iKnowledge.interface';
 
 @Component({
   selector: 'app-knowledge',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './knowledge.component.scss'
 })
 export class KnowledgeComponent {
-
+  public arrayKnowledge = signal<IKnowledge[]>([
+    {
+      src: 'assets/icons/knowledge/html5.svg',
+      alt: 'Ícone de HTML5'
+    },
+    {
+      src: 'assets/icons/knowledge/angular.svg',
+      alt: 'Ícone de Angular'
+    },
+    {
+      src: 'assets/icons/knowledge/nodejs.svg',
+      alt: 'Ícone de NodeJS'
+    },
+  ])
 }
