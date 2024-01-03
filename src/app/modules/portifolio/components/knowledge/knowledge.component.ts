f4d336948bef1699b4205d645186671e2ff4be25
hc-pedrosouza <hc.pedrosouza@gmail.com>
2024-01-02 23:23:16 -0300

    feat: add new knowledgeimages

--- src/app/modules/portifolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portifolio/components/knowledge/knowledge.component.ts
@@ -6,21 +6,49 @@ import { IKnowledge } from '../../interface/iKnowledge.interface';
   standalone: true,
   imports: [],
   templateUrl: './knowledge.component.html',
-  styleUrl: './knowledge.component.scss'
+  styleUrl: './knowledge.component.scss',
 })
 export class KnowledgeComponent {
   public arrayKnowledge = signal<IKnowledge[]>([
     {
-      src: 'assets/icons/knowledge/html5.svg',
-      alt: 'Ícone de HTML5'
-    },
-    {
-      src: 'assets/icons/knowledge/angular.svg',
-      alt: 'Ícone de Angular'
+      src: 'assets/icons/knowledge/typescript.svg',
+      alt: 'Ícone de Typescript',
     },
     {
       src: 'assets/icons/knowledge/nodejs.svg',
-      alt: 'Ícone de NodeJS'
+      alt: 'Ícone de NodeJS',
     },
-  ])
+    {
+      src: 'assets/icons/knowledge/angular.svg',
+      alt: 'Ícone de Angular',
+    },
+    {
+      src: 'assets/icons/knowledge/nestjs.svg',
+      alt: 'Ícone de NestJS',
+    },
+    {
+      src: 'assets/icons/knowledge/cypress.svg',
+      alt: 'Ícone de Cypress',
+    },
+    {
+      src: 'assets/icons/knowledge/docker.svg',
+      alt: 'Ícone de Docker',
+    },
+    {
+      src: 'assets/icons/knowledge/aws.svg',
+      alt: 'Ícone de AWS',
+    },
+    {
+      src: 'assets/icons/knowledge/python.svg',
+      alt: 'Ícone de Python',
+    },
+    {
+      src: 'assets/icons/knowledge/selenium.svg',
+      alt: 'Ícone de Selenium',
+    },
+    {
+      src: 'assets/icons/knowledge/html5.svg',
+      alt: 'Ícone de HTML5',
+    },
+  ]);
 }
